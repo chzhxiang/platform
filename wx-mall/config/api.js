423035bec4f51cecc0a87648c488576789f1d9b3
Ahuanghaifeng <1490738477@qq.com>
2018-10-10 18:35:13 +0800

    购买页面修改，购物车和购买接口传递json

--- wx-mall/config/api.js
+++ wx-mall/config/api.js
@@ -1,7 +1,7 @@
 const root = '/platform/api/';
 
 // const API_BASE_URL = 'http://fly2you.cn' + root;
-const API_BASE_URL = 'http://10.4.0.61:8080' + root;
+const API_BASE_URL = 'http://10.4.0.60:8080' + root;
 module.exports = {
     IndexUrlNewGoods: API_BASE_URL + 'index/newGoods', //新品首发
     IndexUrlHotGoods: API_BASE_URL + 'index/hotGoods', //热卖商品
